9c6e3d143447c23979f0c5e1ec9604ec4faf86f1
Alejandro-Polo <alejandroatarfe2004@gmail.com>
2026-03-06 00:52:43 +0100

    cambios en el contenido dockerfile y main.tf

--- terraform/ec2-docker/main.tf
+++ terraform/ec2-docker/main.tf
@@ -70,21 +70,21 @@ resource "aws_instance" "docker_server" {
  user_data = <<-EOF
               #!/bin/bash
 
-              yum update -y
-              yum install -y docker git
+              apt update -y
+              apt install -y docker.io git
               systemctl start docker
               systemctl enable docker
 
               curl -L "https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
               chmod +x /usr/local/bin/docker-compose
 
-              cd /home/ec2-user
+              cd /home/ubuntu
 
               git clone https://github.com/Alejandro-Polo/pruebadespliegue.git
 
               cd pruebadespliegue
 
-              docker-compose up -d
+              docker-compose up -d --build
 
               EOF
 
