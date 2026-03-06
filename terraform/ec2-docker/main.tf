4b01a945128ffdb6fd2f4482b1a876af3f430d65
Alejandro-Polo <alejandroatarfe2004@gmail.com>
2026-03-06 10:50:04 +0100

    prueba final primer despliegue

--- terraform/ec2-docker/main.tf
+++ terraform/ec2-docker/main.tf
@@ -69,13 +69,17 @@ resource "aws_instance" "docker_server" {
 
  user_data = <<-EOF
               #!/bin/bash
+              set -e
 
               yum update -y
               yum install -y docker git
+
               systemctl start docker
               systemctl enable docker
 
-              curl -L "https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+              usermod -aG docker ec2-user
+
+              curl -L https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
               chmod +x /usr/local/bin/docker-compose
 
               cd /home/ec2-user
@@ -84,7 +88,7 @@ resource "aws_instance" "docker_server" {
 
               cd pruebadespliegue
 
-              docker-compose up -d
+              docker-compose up -d --build
 
               EOF
 
