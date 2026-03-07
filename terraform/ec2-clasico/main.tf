eeaa0f99cd8aefa568bd9a664d4adb830256a235
Alejandro-Polo <alejandroatarfe2004@gmail.com>
2026-03-08 00:16:03 +0100

    deploy y destroy de una ec2 sin usar docker

---
+++ terraform/ec2-clasico/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "classic-ec2-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "classic_ec2" {
+
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  security_groups = [aws_security_group.web_sg.name]
+
+  user_data = file("user-data.sh")
+
+  tags = {
+    Name = "classic-react-symfony"
+  }
+}
---
+++ terraform/ec2-clasico/user-data.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+
+yum update -y
+
+amazon-linux-extras install nginx1 -y
+systemctl start nginx
+systemctl enable nginx
+
+amazon-linux-extras enable php8.2
+yum install php php-cli php-fpm php-mysqlnd git unzip -y
+
+systemctl start php-fpm
+systemctl enable php-fpm
+
+curl -sL https://rpm.nodesource.com/setup_18.x | bash -
+yum install nodejs -y
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php composer-setup.php
+mv composer.phar /usr/local/bin/composer
