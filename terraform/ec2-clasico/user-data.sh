0ac048e232fa92c41665eb033cb5e821db79b288
Alejandro-Polo <alejandroatarfe2004@gmail.com>
2026-03-08 20:37:16 +0100

    instalacion de composer

--- terraform/ec2-clasico/user-data.sh
+++ terraform/ec2-clasico/user-data.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 exec > /var/log/user-data.log 2>&1
 
 yum update -y
 
-amazon-linux-extras install -y nginx1
+amazon-linux-extras install nginx1 -y
 systemctl enable nginx
 systemctl start nginx
 
@@ -15,11 +14,11 @@ yum install -y php php-cli php-fpm php-mysqlnd php-json php-mbstring php-xml git
 systemctl enable php-fpm
 systemctl start php-fpm
 
-yum install -y nodejs npm
+amazon-linux-extras install nodejs18 -y
 
-cd /usr/local/bin
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar composer
+cd /tmp
+curl -sS https://getcomposer.org/installer -o composer-setup.php
+php composer-setup.php --install-dir=/usr/local/bin --filename=composer
 
 yum install -y mariadb-server
 systemctl enable mariadb
@@ -39,9 +38,9 @@ composer install --no-interaction
 
 php bin/console doctrine:migrations:migrate --no-interaction || true
 
-chown -R nginx:nginx /home/ec2-user/pruebadespliegue/backend
-chmod -R 755 /home/ec2-user/pruebadespliegue/backend
-chmod -R 775 /home/ec2-user/pruebadespliegue/backend/var
+chmod o+x /home/ec2-user
+chmod -R 755 /home/ec2-user/pruebadespliegue
+chown -R nginx:nginx /home/ec2-user/pruebadespliegue/backend/var
 
 cd ../frontend
 
@@ -53,34 +52,35 @@ cp -r dist/* /usr/share/nginx/html/
 
 cat > /etc/nginx/conf.d/app.conf <<'EOF'
 server {
+
     listen 80;
     server_name _;
 
     root /usr/share/nginx/html;
     index index.html;
 
-    # React
     location / {
         try_files $uri /index.html;
     }
 
-    # API Symfony
     location /api {
-        alias /home/ec2-user/pruebadespliegue/backend/public;
+        root /home/ec2-user/pruebadespliegue/backend/public;
         try_files $uri /index.php$is_args$args;
     }
 
-    # CRUD Symfony
     location /articulo {
-        alias /home/ec2-user/pruebadespliegue/backend/public;
+        root /home/ec2-user/pruebadespliegue/backend/public;
         try_files $uri /index.php$is_args$args;
     }
 
     location ~ ^/index\.php(/|$) {
+
         root /home/ec2-user/pruebadespliegue/backend/public;
         fastcgi_pass unix:/run/php-fpm/www.sock;
+
         include fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME $document_root/index.php;
+        fastcgi_param SCRIPT_FILENAME $realpath_root$fastcgi_script_name;
+
     }
 }
 EOF
